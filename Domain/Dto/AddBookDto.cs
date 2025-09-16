a483a8c0712a6995d184bd56d577cbf3aab938de
Morphezy <nicebrohehe@gmail.com>
2025-09-16 20:26:03 +0300

    AddBookDto.cs

---
+++ Domain/Dto/AddBookDto.cs
@@ -0,0 +1,8 @@
+namespace Domain.Dto;
+
+public class AddBookDto
+{
+    public string Name { get; private set; }
+    public string Author { get; private set; }
+    public string Publisher { get; private set; }
+}
