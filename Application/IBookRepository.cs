2710161749e0f4323b36315d9bd513d268d90eee
Morphezy <nicebrohehe@gmail.com>
2025-09-16 16:26:10 +0300

    IBookRepo

---
+++ Application/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+
+namespace Application;
+
+public interface IBookRepository
+{
+    public List<Book> GetAll();
+    public Book GetById(int id);
+    public List<Book> GetByAuthor(string author);
+    public List<Book> GetByPublisher(string publisher);
+    public List<Book> GetByName(string name);
+    
+    public Book Add(Book book);
+    
+    public Book Delete(int id);
+}
